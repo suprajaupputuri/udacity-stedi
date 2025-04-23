b56ca1328ab5b02108064b9bcc4acf1fbe37e20f
suprajaupputuri <128447615+suprajaupputuri@users.noreply.github.com>
2025-04-23 08:42:52 +0530

    Update Accelerometer Landing to Trusted.py

--- Accelerometer Landing to Trusted.py
+++ Accelerometer Landing to Trusted.py
@@ -21,21 +21,21 @@ DEFAULT_DATA_QUALITY_RULESET = """
 """
 
 # Script generated for node Accelerometer Landing
-AccelerometerLanding_node1745326745641 = glueContext.create_dynamic_frame.from_catalog(database="stedi_db", table_name="accelerometer_landing", transformation_ctx="AccelerometerLanding_node1745326745641")
+AccelerometerLanding_node1 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_landing", transformation_ctx="AccelerometerLanding_node1")
 
 # Script generated for node Customer Trusted
-CustomerTrusted_node1745326748955 = glueContext.create_dynamic_frame.from_catalog(database="stedi_db", table_name="customer_trusted", transformation_ctx="CustomerTrusted_node1745326748955")
+CustomerTrusted_node1 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_trusted", transformation_ctx="CustomerTrusted_node1")
 
 # Script generated for node Join
-Join_node1745326856033 = Join.apply(frame1=AccelerometerLanding_node1745326745641, frame2=CustomerTrusted_node1745326748955, keys1=["user"], keys2=["email"], transformation_ctx="Join_node1745326856033")
+Join_node1 = Join.apply(frame1=AccelerometerLanding_node1, frame2=CustomerTrusted_node1, keys1=["user"], keys2=["email"], transformation_ctx="Join_node1")
 
 # Script generated for node Drop Fields
-DropFields_node1745326929497 = DropFields.apply(frame=Join_node1745326856033, paths=["email", "phone", "customername", "birthday", "serialnumber", "registrationdate", "lastupdatedate", "sharewithresearchasofdate", "sharewithpublicasofdate", "sharewithfriendsasofdate"], transformation_ctx="DropFields_node1745326929497")
+DropFields_node1 = DropFields.apply(frame=Join_node1, paths=["email", "phone", "customername", "birthday", "serialnumber", "registrationdate", "lastupdatedate", "sharewithresearchasofdate", "sharewithpublicasofdate", "sharewithfriendsasofdate"], transformation_ctx="DropFields_node1")
 
 # Script generated for node Accelerometer Trusted
-EvaluateDataQuality().process_rows(frame=DropFields_node1745326929497, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1745326395505", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
-AccelerometerTrusted_node1745327006721 = glueContext.getSink(path="s3://stedi-lake-house-my-1/accelerometer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AccelerometerTrusted_node1745327006721")
-AccelerometerTrusted_node1745327006721.setCatalogInfo(catalogDatabase="stedi_db",catalogTableName="accelerometer_trusted")
-AccelerometerTrusted_node1745327006721.setFormat("json")
-AccelerometerTrusted_node1745327006721.writeFrame(DropFields_node1745326929497)
+EvaluateDataQuality().process_rows(frame=DropFields_node1, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1745326395505", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AccelerometerTrusted_node1 = glueContext.getSink(path="s3://stedi-supraja/accelerometer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AccelerometerTrusted_node1")
+AccelerometerTrusted_node1.setCatalogInfo(catalogDatabase="stedi_db",catalogTableName="accelerometer_trusted")
+AccelerometerTrusted_node1.setFormat("json")
+AccelerometerTrusted_node1.writeFrame(DropFields_node1)
 job.commit()
