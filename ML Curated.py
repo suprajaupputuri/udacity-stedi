58962b461c0f41ba8d74914504101a06ced4c46f
suprajaupputuri <128447615+suprajaupputuri@users.noreply.github.com>
2025-04-23 08:55:20 +0530

    Update ML Curated.py

--- ML Curated.py
+++ ML Curated.py
@@ -27,13 +27,13 @@ DEFAULT_DATA_QUALITY_RULESET = """
 """
 
 # Script generated for node Accelerometer Trusted
-AccelerometerTrusted_node1745328894406 = glueContext.create_dynamic_frame.from_catalog(database="stedi_db", table_name="accelerometer_trusted", transformation_ctx="AccelerometerTrusted_node1745328894406")
+AccelerometerTrusted_node1 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_trusted", transformation_ctx="AccelerometerTrusted_node1")
 
 # Script generated for node Step_trained Trusted
-Step_trainedTrusted_node1745328895291 = glueContext.create_dynamic_frame.from_catalog(database="stedi_db", table_name="step_trainer_trusted", transformation_ctx="Step_trainedTrusted_node1745328895291")
+Step_trainedTrusted_node1 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="step_trainer_trusted", transformation_ctx="Step_trainedTrusted_node1")
 
 # Script generated for node SQL Query
-SqlQuery820 = '''
+SqlQuery720 = '''
 select step_trained_trusted.sensorreadingtime,
 step_trained_trusted.serialnumber,
 step_trained_trusted.distancefromobject,
@@ -46,12 +46,12 @@ join accelerometer_trusted
 on accelerometer_trusted.timestamp=step_trained_trusted.sensorreadingtime;
 
 '''
-SQLQuery_node1745329007720 = sparkSqlQuery(glueContext, query = SqlQuery820, mapping = {"accelerometer_trusted":AccelerometerTrusted_node1745328894406, "step_trained_trusted":Step_trainedTrusted_node1745328895291}, transformation_ctx = "SQLQuery_node1745329007720")
+SQLQuery_node1 = sparkSqlQuery(glueContext, query = SqlQuery720, mapping = {"accelerometer_trusted":AccelerometerTrusted_node1, "step_trained_trusted":Step_trainedTrusted_node1}, transformation_ctx = "SQLQuery_node1")
 
 # Script generated for node ML Curated
-EvaluateDataQuality().process_rows(frame=SQLQuery_node1745329007720, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1745328849154", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
-MLCurated_node1745329318023 = glueContext.getSink(path="s3://stedi-lake-house-my-1/curated/ml_curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="MLCurated_node1745329318023")
-MLCurated_node1745329318023.setCatalogInfo(catalogDatabase="stedi_db",catalogTableName="ml_curated")
-MLCurated_node1745329318023.setFormat("json")
-MLCurated_node1745329318023.writeFrame(SQLQuery_node1745329007720)
+EvaluateDataQuality().process_rows(frame=SQLQuery_node1, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+MLCurated_node1 = glueContext.getSink(path="s3://stedi-supraja/machine_learning/curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="MLCurated_node1")
+MLCurated_node1.setCatalogInfo(catalogDatabase="stedi",catalogTableName="ml_curated")
+MLCurated_node1.setFormat("json")
+MLCurated_node1.writeFrame(SQLQuery_node1)
 job.commit()
