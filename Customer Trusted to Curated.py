d2be368760dc1906bafdae902504e8da8c675945
suprajaupputuri <128447615+suprajaupputuri@users.noreply.github.com>
2025-04-23 08:49:15 +0530

    Update Customer Trusted to Curated.py

--- Customer Trusted to Curated.py
+++ Customer Trusted to Curated.py
@@ -27,10 +27,10 @@ DEFAULT_DATA_QUALITY_RULESET = """
 """
 
 # Script generated for node Customer Trusted
-CustomerTrusted_node1745327515740 = glueContext.create_dynamic_frame.from_catalog(database="stedi_db", table_name="customer_trusted", transformation_ctx="CustomerTrusted_node1745327515740")
+CustomerTrusted_node1 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_trusted", transformation_ctx="CustomerTrusted_node1")
 
 # Script generated for node Accelerometer landing
-Accelerometerlanding_node1745327518076 = glueContext.create_dynamic_frame.from_catalog(database="stedi_db", table_name="accelerometer_landing", transformation_ctx="Accelerometerlanding_node1745327518076")
+Accelerometerlanding_node1 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_landing", transformation_ctx="Accelerometerlanding_node1")
 
 # Script generated for node SQL Query
 SqlQuery757 = '''
@@ -40,12 +40,12 @@ inner join customer_trusted
 on accelerometer_landing.user=customer_trusted.email;
 
 '''
-SQLQuery_node1745327591949 = sparkSqlQuery(glueContext, query = SqlQuery757, mapping = {"customer_trusted":CustomerTrusted_node1745327515740, "accelerometer_landing":Accelerometerlanding_node1745327518076}, transformation_ctx = "SQLQuery_node1745327591949")
+SQLQuery_node1 = sparkSqlQuery(glueContext, query = SqlQuery757, mapping = {"customer_trusted":CustomerTrusted_node1, "accelerometer_landing":Accelerometerlanding_node1}, transformation_ctx = "SQLQuery_node1")
 
 # Script generated for node Customer Curated
-EvaluateDataQuality().process_rows(frame=SQLQuery_node1745327591949, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1745327486719", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
-CustomerCurated_node1745327720614 = glueContext.getSink(path="s3://stedi-lake-house-my-1/customer/curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="CustomerCurated_node1745327720614")
-CustomerCurated_node1745327720614.setCatalogInfo(catalogDatabase="stedi_db",catalogTableName="customer_curated")
-CustomerCurated_node1745327720614.setFormat("json")
-CustomerCurated_node1745327720614.writeFrame(SQLQuery_node1745327591949)
+EvaluateDataQuality().process_rows(frame=SQLQuery_node1, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+CustomerCurated_node1 = glueContext.getSink(path="s3://stedi-supraja/customer/curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="CustomerCurated_node1")
+CustomerCurated_node1.setCatalogInfo(catalogDatabase="stedi",catalogTableName="customer_curated")
+CustomerCurated_node1.setFormat("json")
+CustomerCurated_node1.writeFrame(SQLQuery_node1)
 job.commit()
